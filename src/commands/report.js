1863a9a8aae67869439b82c76f7cb2dd2b70284c
Ron Wilson <rwilson1@underarmour.com>
2019-04-03 15:45:53 -0400

    add weekly ops report feature

--- src/commands/report.js
+++ src/commands/report.js
@@ -1,10 +1,35 @@
 const {Command, flags} = require('@oclif/command')
 
+const sqlite3 = require('better-sqlite3')
+const config = require('config')
+const converter = require('json-2-csv')
+
+function report1(db) {
+  const rows = db.prepare(`select tdays, count(issue_) as count
+    from opsMeasure natural join issues i
+    where i.issueResolutionStamp between '2019-03-28' and '2019-04-04'
+    group by tdays order by tdays;`).all()
+  converter.json2csvAsync(rows, {delimiter: {field: '\t'}})
+  .then(r => {
+    console.log(r)
+  })
+}
+
 class ReportCommand extends Command {
   async run() {
     const {flags} = this.parse(ReportCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/rwilson1/src/manage/jerg/src/commands/report.js`)
+    const id = flags.id || 0
+    const database = flags.db || (config.has('database') ? config.get('database') : ':memory:')
+    const db = sqlite3(database, {})
+
+    switch (id) {
+    case 'weekly':
+    case '1':
+      report1(db)
+      break
+    default:
+      this.error(`report id not implemented : ${id}`)
+    }
   }
 }
 
@@ -14,7 +39,8 @@ Extra documentation goes here
 `
 
 ReportCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
+  db: flags.string({char: 'd', description: 'database to fill'}),
+  id: flags.string({char: 'i', description: 'id of report to generate'}),
 }
 
 module.exports = ReportCommand
