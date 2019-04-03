3408b5519cf6e9f33596df0697e0895e6ca0e0b7
Ron Wilson <rwilson1@underarmour.com>
2019-04-03 00:17:11 -0400

    remove vestigial tests

--- test/commands/issues.test.js
+++ test/commands/issues.test.js
@@ -1,17 +1,4 @@
-const {expect, test} = require('@oclif/test')
+// const {expect, test} = require('@oclif/test')
 
 describe('issues', () => {
-  test
-  .stdout()
-  .command(['issues'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['issues', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
 })
--- test/commands/metadata.test.js
+++ test/commands/metadata.test.js
@@ -1,17 +1,4 @@
-const {expect, test} = require('@oclif/test')
+// const {expect, test} = require('@oclif/test')
 
 describe('metadata', () => {
-  test
-  .stdout()
-  .command(['metadata'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['metadata', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
 })
